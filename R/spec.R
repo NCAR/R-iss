421df9294659d28e52039f15d7c98f626d476393
Gordon Maclean <maclean@users.noreply.github.com>
2020-09-11 13:57:36 -0600

    Add doppler velocity and specWid from moments

--- R/spec.R
+++ R/spec.R
@@ -38,6 +38,10 @@ readspec <- function(file="01-Oct-2019_23-45-01.spc.nc",
     # fill value = -9999
     spec <- readnc(iod,"spectraDbs")
     hts <- readnc(iod,"heights")
+    ipp <- readnc(iod,"interPulsePer") * 1.e-9  # convert from nsec to sec
+    nci <- readnc(iod,"numCohIntegs")
+    # freqset <- readnc(iod,"frequency_set")
+    freq <- readnc(iod,"frequency") * 1.e6  # convert from MHz to Hz
 
     if (reorder) {
         nfft <- dim(spec)[1]
@@ -45,15 +49,44 @@ readspec <- function(file="01-Oct-2019_23-45-01.spc.nc",
         spec <- spec[c((mid+2):nfft,1:(mid+1)),,]
     }
 
-    list(spec=spec, hts=hts)
+    list(spec=spec, hts=hts, ipp=ipp, nci=nci, freq=freq)
 }
 
-plotspec <- function(file="01-Oct-2019_23-45-01.spc.nc",
+readmom <- function(file="01-Oct-2019_23-45-01.spc.nc",
+    dir=Sys.getenv("NETCDF_DIR"))
+{
+    require("eolts")
+
+    # options(time.zone="UTC")
+    # dpar(start=utime("2019 10 1 23:45"),lenmin=15)
+    # dpar(start=utime("2018 10 1 23:45"),lenday=10000)
+    iod <- netcdf(file=file,dir=dir)
+    on.exit(close(iod), add=TRUE)
+
+    vel <- readnc(iod,"vel")
+    specWid <- readnc(iod,"specWid")
+
+    list(vel=vel, specWid=specWid)
+}
+
+nyquistvel <- function(ipp,nci,freq)
+{
+    # compute nyquist (highest resolvable) velocity in m/s
+    # from inter-pulse period in nanosec, number of coherent
+    # integrations and the transmitter frequency in MHz.
+    
+    vel <- 1 / (2.0 * nci * ipp) * 2.998e8 / (2.0 * freq)
+}
+
+plotspec <- function(
+    spcfile="01-Oct-2019_23-45-01.spc.nc",
+    momfile="moments_20191001.nc",
     dir=Sys.getenv("NETCDF_DIR"),
     reorder=TRUE, xscale=1.0)
 {
 
-    x <- readspec(file=file,dir=dir,reorder=reorder)
+    x <- readspec(file=spcfile,dir=dir,reorder=reorder)
+    xm <- readmom(file=momfile,dir=dir)
 
     nfft <- dim(x$spec)[1]
     nhts <- dim(x$hts)[1]
@@ -66,12 +99,16 @@ plotspec <- function(file="01-Oct-2019_23-45-01.spc.nc",
     par(mar=c(2.0,2,1.0,0.2), mgp=c(1.0,0.3,0))
 
     for (it in 1:ntimes) {
+        nyvel <- nyquistvel(x$ipp[it], x$nci[it], x$freq)
+        df <- nyvel / nfft * 2
+        # if (it == 1) browser()
+        vel <- -nyvel + df * 0:(nfft-1)
         dhts <- diff(x$hts[, it])
         hmin <- x$hts[1,it] - dhts[1] * 0.30
         hmax <- x$hts[nhts,it] + dhts[length(dhts)] * 1.30
         # set up plot scales and plot the axes but no data
-        plot(vel[c(1,length(vel))]*xscale,c(hmin,hmax), type="n", 
-            xlab="vel", xaxs="i",
+        plot(c(-nyvel,nyvel)*xscale,c(hmin,hmax), type="n", 
+            xlab="vel (m/s)", xaxs="i",
             ylab="", yaxs="i") 
         for (iht in 1:nhts) {
             y0 <- x$hts[iht, it]
@@ -80,10 +117,20 @@ plotspec <- function(file="01-Oct-2019_23-45-01.spc.nc",
             ds <- diff(range(sd, na.rm=TRUE))
             sd <- y0 + dh * sd / ds
             lines(vel,sd)
+
+            dopvel <- xm$vel[iht, it]
+            points(dopvel, y0 + dh * 0.5, pch="+", col="red")
+            velwid <- c(dopvel-0.5*xm$specWid[iht,it],
+                dopvel+0.5*xm$specWid[iht,it])
+            barpts <- c(rep(velwid[1],3),rep(velwid[2],3))
+            ymid <- y0 + dh * 0.5
+            ypts <- c(y0, y0+dh, ymid, ymid, y0, y0+dh)
+            lines(barpts, ypts, col="red")
         }
         # write file name at top of page
         if (all(par("mfg")[1:2] == c(1,1)))
-            mtext(file,side=3,line=-0.9,outer=TRUE,cex=0.8)
+            mtext(paste(spcfile,momfile,sep=", "),
+                side=3,line=-0.9,outer=TRUE,cex=0.8)
     }
 }
 
