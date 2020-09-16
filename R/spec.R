628d75c5d077b7527331c753d9d546cd854d5716
Gordon Maclean <maclean@users.noreply.github.com>
2020-09-15 19:03:00 -0600

    Use %X date descriptors in file, add ndev arg

--- R/spec.R
+++ R/spec.R
@@ -11,29 +11,34 @@ testplots <- function()
 {
     pdf("/tmp/mapr_spec.pdf",width=8,height=10.5)
     par(mfrow=c(6,4))
-    plotspec("01-Oct-2019_23-45-01.spc.nc")
+    plotspec("%d-%b-%Y_%H-%M-%S.spc.nc")
     dev.off()
     pdf("/tmp/nima_spec.pdf",width=8,height=10.5)
     par(mfrow=c(6,4))
     plotspec("spc_20191001.nc")
     dev.off()
-    specdiff(f1="01-Oct-2019_23-45-01.spc.nc",
+    specdiff(f1="%d-%b-%Y_%H-%M-%S.spc.nc",
         f2="spc_20191001.nc")
 }
 
-readspec <- function(file="01-Oct-2019_23-45-01.spc.nc",
+readspec <- function(file="%d-%b-%Y_%H-%M-%S.spc.nc",
     dir=Sys.getenv("NETCDF_DIR"))
 {
     require("eolts")
 
-    options(time.zone="UTC")
-    # dpar(start=utime("2019 10 1 23:45"),lenmin=15)
-    dpar(start=utime("2018 10 1 23:45"),lenday=10000)
+    start <- dpar("start")
+    end <- dpar("end")
+
+    # read a longer period of times, by two hours
+    dpar(start=start-3600)
+    dpar(end=end+3600)
+    on.exit(dpar(start=start, end=end),add=TRUE)
+
     iod <- netcdf(file=file,dir=dir)
     on.exit(close(iod), add=TRUE)
 
     #vars <- variables(iod)
-    #vars <- variables(iod, all=TRUE)
+    # vars <- variables(iod, all=TRUE)
 
     # in netcdf, rightmost, last dimension varies most rapidly
     # numSpecAvg:  20
@@ -46,16 +51,33 @@ readspec <- function(file="01-Oct-2019_23-45-01.spc.nc",
 
     # fill value = -9999
 
+    time <- readts(iod,"time")
+    dpar(start=start, end=end)
+    nt <- nrow(time)
+    itimes <- positions(time) >= start & positions(time) <= end
+    if (!any(itimes)) {
+        cat(paste(file,": start time=",format(positions(time[1,])),", end=",
+                    format(positions(time[nt,]))),"\n",sep="")
+
+        cat(paste("dpar: start=",format(dpar("start")),", end=",format(dpar("end")),"\n",sep=""))
+        stop("times in file not in dpar range")
+    }
+
+    time <- time[itimes,,drop=FALSE]
+
+    close(iod)
+    iod <- netcdf(file=file,dir=dir)
+
+    spec <- readnc(iod,"spectraDbs")[,,itimes,drop=FALSE]
+    hts <- readnc(iod,"heights")[,itimes,drop=FALSE]
+    ipp <- readnc(iod,"interPulsePer")[itimes,drop=FALSE] * 1.e-9  # convert from nsec to sec
+    nci <- readnc(iod,"numCohIntegs")[itimes,drop=FALSE]
+    # freqset <- readnc(iod,"frequency_set")
+    freq <- readnc(iod,"frequency") * 1.e6  # convert from MHz to Hz
+
     gattrs <- readnc(iod)
     reorder <- !is.null(gattrs$history) &&
         length(grep("wppp",gattrs$history,fixed=TRUE)) == 0
-
-    spec <- readnc(iod,"spectraDbs")
-    hts <- readnc(iod,"heights")
-    ipp <- readnc(iod,"interPulsePer") * 1.e-9  # convert from nsec to sec
-    nci <- readnc(iod,"numCohIntegs")
-    # freqset <- readnc(iod,"frequency_set")
-    freq <- readnc(iod,"frequency") * 1.e6  # convert from MHz to Hz
 
     if (reorder) {
         nfft <- dim(spec)[1]
@@ -63,10 +85,10 @@ readspec <- function(file="01-Oct-2019_23-45-01.spc.nc",
         spec <- spec[c((mid+2):nfft,1:(mid+1)),,]
     }
 
-    list(spec=spec, hts=hts, ipp=ipp, nci=nci, freq=freq)
+    list(spec=spec, hts=hts, ipp=ipp, nci=nci, freq=freq, time=time)
 }
 
-readmom <- function(file="moments_20191001.nc",
+readmom <- function(file="moments_%Y%m%d.nc",
     dir=Sys.getenv("NETCDF_DIR"))
 {
     require("eolts")
@@ -93,22 +115,27 @@ nyquistvel <- function(ipp,nci,freq)
 }
 
 plotspec <- function(
-    spcfile="01-Oct-2019_23-45-01.spc.nc",
-    momfile=NULL,
+    spcfile="%d-%b-%Y_%H-%M-%S.spc.nc",
+    momfile="moments_%Y%m%d.nc",
     dir=Sys.getenv("NETCDF_DIR"),
     xscale=1.0,
-    db=FALSE)
+    db=FALSE,
+    ndev=1)
 {
+    # ndev:
+    #   1: one device, overplot all plots on same device
+    #   N: open up to N new devices
+
     require("lattice")
 
     x <- readspec(file=spcfile,dir=dir)
 
-    if (!is.null(momfile)) xm <- readmom(file=momfile,dir=dir)
-    else xm <- NULL
-
     nfft <- dim(x$spec)[1]
     nhts <- dim(x$hts)[1]
     ntimes <- dim(x$hts)[2]
+
+    if (!is.null(momfile)) xm <- readmom(file=momfile,dir=dir)
+    else xm <- NULL
 
     mid <- trunc(nfft/2)
     vel <- (-mid+1):mid
@@ -116,8 +143,13 @@ plotspec <- function(
     # tighten margins
     par(mar=c(2.0,2,1.0,0.2), mgp=c(1.0,0.3,0))
 
-    ntimes <- 1
+    iplot <- 0
     for (it in 1:ntimes) {
+        iplot <- iplot + 1
+        ncdev <- length(dev.list())
+        if (iplot > ncdev && ncdev < ndev) dev.new()
+        else dev.set()
+
         nyvel <- nyquistvel(x$ipp[it], x$nci[it], x$freq)
         df <- nyvel / nfft * 2
         # if (it == 1) browser()
@@ -164,13 +196,13 @@ plotspec <- function(
             }
             plot(levelplot(if (db) 10 * log10(x$spec[,,it]) else x$spec[,,it],
                 row.values=vel, column.values=x$hts[,it], aspect="fill",
-                xlab="vel(m)", ylab="heigth(m)",
-                main=paste(paste(spcfile, momfile,sep=", "),
+                xlab="vel(m/s)", ylab="heigth(m)",
+                main=paste(format(positions(x$time[it,])),
                     if (db) "(db)" else ""),
+                sub=paste(paste(spcfile, momfile,sep=", ")),
                 panel=function(...) {
                     panel.levelplot(...)
                     mypanel.lines(...)
-
                 }
                 ))
         }
@@ -204,16 +236,19 @@ plotspec <- function(
                     side=3,line=-0.9,outer=TRUE,cex=0.8)
         }
     }
+    invisible(NULL)
 }
 
-specdiff <- function(f1="01-Oct-2019_23-45-01.spc.nc",
-    f2="spcmom_20191001.nc",
+specdiff <- function(f1="%d-%b-%Y_%H-%M-%S.spc.nc",
+    f2="spcmom_%Y%m%d.nc",
     dir="/home/maclean/ncar/nima/test")
 {
 
     x1 <- readspec(file=f1,dir=dir)
     x2 <- readspec(file=f2,dir=dir)
 
+    cat(paste(f1,", ",f2,": dimensions= ",
+        paste(dim(x1$spec),collapse="x"),"  ",paste(dim(x2$spec),collapse="x"),"\n",sep=""))
     cat(paste(f1,", ",f2,": max diff=",
         max(x1$spec-x2$spec, na.rm=TRUE),"\n",sep=""))
     cat(paste(f1,", ",f2,": NAs equal=",
