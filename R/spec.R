95bb92382fd6e6e99c37c2810dcc2d024c29bf06
Gordon Maclean <maclean@users.noreply.github.com>
2020-09-30 15:48:11 -0600

    Quick add of fuzzy plots

--- R/spec.R
+++ R/spec.R
@@ -105,6 +105,61 @@ readmom <- function(file="moments_%Y%m%d.nc",
     list(vel=vel, specWid=specWid)
 }
 
+readfuzzy <- function(file="fuzzy_%Y%m%d.nc",
+    dir=Sys.getenv("NETCDF_DIR"))
+{
+    require("eolts")
+
+    start <- dpar("start")
+    end <- dpar("end")
+
+    # read a longer period of times, by two hours
+    dpar(start=start-3600)
+    dpar(end=end+3600)
+    on.exit(dpar(start=start, end=end),add=TRUE)
+
+    iod <- netcdf(file=file,dir=dir)
+    on.exit(close(iod), add=TRUE)
+
+    time <- readts(iod,"time")
+    dpar(start=start, end=end)
+    nt <- nrow(time)
+    itimes <- positions(time) >= start & positions(time) <= end
+    if (!any(itimes)) {
+        cat(paste(file,": start time=",format(positions(time[1,])),", end=",
+                    format(positions(time[nt,]))),"\n",sep="")
+
+        cat(paste("dpar: start=",format(dpar("start")),", end=",format(dpar("end")),"\n",sep=""))
+        stop("times in file not in dpar range")
+    }
+
+    time <- time[itimes,,drop=FALSE]
+
+    close(iod)
+    iod <- netcdf(file=file,dir=dir)
+
+    vars <- variables(iod, all=TRUE)
+
+    clutter <- NULL
+    if ("clutter_features" %in% vars) 
+        clutter <- readnc(iod,"clutter_features")[,,itimes,drop=FALSE]
+
+    doppler <- NULL
+    if ("doppler_peak_features" %in% vars) 
+        doppler <- readnc(iod,"doppler_peak_features")[,,itimes,drop=FALSE]
+
+    rfi <- NULL
+    if ("rfi_features" %in% vars) 
+        rfi <- readnc(iod,"rfi_features")[,,itimes,drop=FALSE]
+
+    hts <- readnc(iod,"heights")[,itimes,drop=FALSE]
+    ipp <- readnc(iod,"interPulsePer")[itimes,drop=FALSE] * 1.e-9  # convert from nsec to sec
+    nci <- readnc(iod,"numCohIntegs")[itimes,drop=FALSE]
+    # freqset <- readnc(iod,"frequency_set")
+    freq <- readnc(iod,"frequency") * 1.e6  # convert from MHz to Hz
+    list(clutter=clutter, doppler=doppler, rfi=rfi,
+        hts=hts, ipp=ipp, nci=nci, freq=freq, time=time)
+}
 nyquistvel <- function(ipp,nci,freq)
 {
     # compute nyquist (highest resolvable) velocity in m/s
@@ -234,6 +289,106 @@ plotspec <- function(
                 right=list(pad1=pd,pad2=pd),left=list(pad1=pd,pad2=pd)))
         )
         plots[[iplot]] <- plt
+    }
+
+    # print the plots, using layout and ndev
+    lapply(1:length(plots),function(i) {
+        nperpage <- prod(layout)
+        ipage <- trunc((i - 1) / nperpage) + 1
+        ndevs <- length(dev.list())
+        ip <- (i-1) %% nperpage + 1
+        if (ipage > ndevs && ipage <= ndev) dev.new()
+        else if (ip == 1 && ipage > 1) dev.set()
+
+        lyout <- c(trunc((ip-1)/layout[2])+1,((ip-1)%%layout[2]+1),layout)
+        more <- ip < nperpage && i < length(plots)
+        if (debug) cat("lyout=",paste(lyout,collapse=","),", more=",more,"\n")
+        print(plots[[i]],split=lyout, more=more)
+        NULL
+    })
+    # If the lapply function above returns the value of print(), then
+    # the list returned by lapply is identical to the plots list
+
+    # return plot objects
+    invisible(plots)
+}
+
+plotfuzzy <- function(
+    fuzzyfile="fuzzy_%Y%m%d.nc",
+    dir=Sys.getenv("NETCDF_DIR"),
+    vars=c("clutter","doppler","rfi"),
+    xzoom=1.0,          # expansion of velocity scale
+    layout=c(1,1),      # nx,ny layout on page
+    palette="Heat2",    # color palette
+    contour=FALSE,      # whether to add contour lines
+    ndev=1,             # how many devices (windows) to ope
+    debug=FALSE
+    )
+{
+    # ndev:
+    #   1: one device, overplot all plots on same device
+    #   N: open up to N new devices
+
+    require("lattice")
+
+    x <- readfuzzy(file=fuzzyfile,dir=dir)
+
+    # tighten margins
+    # par(mar=c(2.0,2,1.0,0.2), mgp=c(1.0,0.3,0))
+
+    plots <- list()
+    iplot <- 0
+
+    ntimes <- dim(x$hts)[2]
+
+    for (it in 1:ntimes) {
+        iplot <- iplot + 1
+
+        for (var in vars) {
+
+            # If one time was selected in NetCDF file, values
+            # will have 2 dimensions else 3
+            if (length(dim(x[[var]])) == 3) vals <- x[[var]][,,it]
+            else vals <- x[[var]]
+
+            nfft <- dim(vals)[1]
+            nhts <- dim(x$hts)[1]
+
+            mid <- trunc(nfft/2)
+            vel <- (-mid+1):mid
+
+
+            nyvel <- nyquistvel(x$ipp[it], x$nci[it], x$freq)
+            df <- nyvel / nfft * 2
+            # if (it == 1) browser()
+            vel <- -nyvel + df * 0:(nfft-1)
+            dhts <- diff(x$hts[, it])
+            hmin <- x$hts[1,it] - dhts[1] * 0.30
+            hmax <- x$hts[nhts,it] + dhts[length(dhts)] * 1.30
+
+            pd <- -0.2  # reduce padding between axis titles, labels, tics
+            plt <- levelplot(vals,
+                row.values=vel, column.values=x$hts[,it], aspect="fill",
+                xlab="vel(m/s)", ylab="heigth(m)",
+                xlim=range(vel) / xzoom,
+                main=list(label=paste(var,format(positions(x$time[it,]))),
+                     cex=0.8),
+                sub=list(label=fuzzyfile,cex=0.7),
+                panel=function(...) {
+                    panel.levelplot(...)
+                },
+                scales=list(y=list(rot=c(90,-90))),
+                # this rotates the colorkey labels, but they are not
+                # centered on the tics.  Don't know how to fix that.
+                colorkey=list(labels=list(rot=90)),
+                col.regions=hcl.colors(100,palette=palette,rev=TRUE),
+                contour=contour,
+                par.settings=list(axis.components=list(
+                    bottom=list(pad1=pd,pad2=pd),top=list(pad1=pd,pad2=pd),
+                    right=list(pad1=pd,pad2=pd),left=list(pad1=pd,pad2=pd)))
+            )
+            plots[[iplot]] <- plt
+        }
     }
 
     # print the plots, using layout and ndev
